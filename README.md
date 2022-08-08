c5a2f3d8f881137469943de2d069918c0195c723
Pratiksha123 <parihari.pratiksha17@gmail.com>
2022-08-08 22:50:10 +0530

    Assignment-nodejs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # thorium
 Backend cohort Feb 2022 - May 2022
+
+https://docs.google.com/document/d/1HanTHEnUW0d2M7KOCTWrnqzwRWOkLUU0X_x_TwWnyTs/edit
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const externalModule = require("../logger/logger.js");
 const dateInfo = require("../util/helper.js");
-const validatorEx = require("../validator/formatter.js")
+const validatorEx = require("../validator/formatter.js");
+
+const loadash = require('lodash');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
@@ -14,6 +16,32 @@ router.get('/test-me', function (req, res) {
     validatorEx.afterTrim();
     validatorEx.changeToUpperCase();
     validatorEx.changetoLowerCase();
+
+    //chunk function
+    let monthsOfYear = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August','September', 'October', 'November','December'];
+    let subarrays = loadash.chunk(monthsOfYear,3);
+    console.log(subarrays);
+
+    //tail function
+    let oddNumber = [1,3,5,7,9,11,13,15,17,19];
+    let last9Elements = loadash.tail(oddNumber);
+    console.log("Last 9 elements are: " ) 
+    console.log(last9Elements);
+
+    //union function
+    let arr1 = [ 1, 2, 3, 4, 5 ];
+   let arr2 = [ 4, 3, 8,6 ];
+   let arr3 = [ 8, 39, 67, 6];
+   let arr4 = [ 39, 87, 34, 90 ];
+   let arr5 = [ 87, 45, 36];
+   console.log("merged and unique values are: " )
+   console.log(loadash.union(arr1,arr2,arr3,arr4,arr5));
+
+   //fromPairs function
+   let movies = [ ["horror","The Shining"],["drama","Titanic" ], ["thriller", "Shutter Island"],["fantasy","Pans Labyrinth"]];
+     console.log(loadash.fromPairs(movies));
+
+
 
 });
 
